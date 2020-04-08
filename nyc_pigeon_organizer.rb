07ccbd5b3ee0f8719cbb87e7527ce6dd5b96d1a3
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-08 16:05:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.reduce({}) do |memo, (key, value)|
   #sorted_member_list =  bands.reduce({}) do |memo, (key, value)|
- memo[value] = key.sort
+ memo[key] = value.sort
  p memo
 
   end
